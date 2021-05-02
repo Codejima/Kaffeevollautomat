b9caa9f002ec17b542d4af762a724cad42bb25b8
goddygodsen <misiu-krecik@web.de>
2021-05-02 14:47:59 +0200

    implemented a new CheckReserve

--- Logic.cs
+++ Logic.cs
@@ -16,37 +16,32 @@ namespace Kaffeevollautomat
         {
             DisplayReserve();
             Console.WriteLine("Was möchten Sie?");
-            Console.WriteLine("1 - Latte\n2 - Capu\n3 - Espresso\n4 - Schwarz\n5 - Reinigung starten\n6 - Maschine ausschalten"); //alles (inkl auswahl) anzeigen. keine untermenüs
+            Console.WriteLine("1 - Latte\n2 - Capu\n3 - Espresso\n4 - Schwarz\n5 - Reinigung starten\n6 - Maschine ausschalten");
             string choice = Console.ReadLine(); //main
             int choiceAsInt;
             choiceAsInt = int.Parse(choice);
-
+            //CheckReserve();
+            Sortiment(choiceAsInt);
+            Idle();
+        }
+        public void Sortiment(int choiceAsInt)
+        {
             switch (choiceAsInt)
             {
                 case 1:
-                    CheckReserve();
-                    Sortiment(1);
-                    Idle();
+                    CheckReserve(2,8,4);
                     break;
                 case 2:
-                    CheckReserve();
-                    Sortiment(2);
-                    Idle();
+                    CheckReserve(2,4,4);
                     break;
                 case 3:
-                    CheckReserve();
-                    Sortiment(3);
-                    Idle();
+                    CheckReserve(4,0,3);
                     break;
                 case 4:
-                    CheckReserve();
-                    Sortiment(4);
-                    Idle();
+                    CheckReserve(3, 0, 4);
                     break;
                 case 5:
-                    CheckReserve();
                     Reinigung();
-                    Idle();
                     break;
                 case 6:
                     Shutdown();
@@ -54,79 +49,122 @@ namespace Kaffeevollautomat
                 default:
                     break;
             }
-
         }
-
-        public void Sortiment(int choiceAsInt)
-        {
-            //Console.WriteLine("Wählen Sie ihren Kaffe:");
-            //Console.WriteLine("1 - Latte\n2 - Capu\n3 - Espresso\n4 - Schwarz");
-            //string choice = Console.ReadLine();
-            //int choiceAsInt;
-            //choiceAsInt = int.Parse(choice);
-
-            switch (choiceAsInt)
-            {
-                case 1:
-                    Console.WriteLine("Latte wird zubereitet.");
-                    Reserve(2, 8, 4);
-                    break;
-                case 2:
-                    Console.WriteLine("Capu wird zubereitet.");
-                    Reserve(2, 4, 4);
-                    break;
-                case 3:
-                    Console.WriteLine("Espresso wird zubereitet.");
-                    Reserve(4, 0, 3);
-                    break;
-                case 4:
-                    Console.WriteLine("Schwarz wird zubereitet.");
-                    Reserve(3, 0, 4);
-                    break;
-                default:
-                    break;
-            }
-        }
-        int coffee = 75; //75
-        int milk = 20; //20
+        int coffee = 1; //75
+        int milk = 1; //20
         int water = 90; //90
         public void Reserve(int c, int m, int w)
         {
             coffee -= c;
             milk -= m;
             water -= w;
+            
+            //Recipes
+            //Latte: 2, 8, 4
+            //Capu: 2, 4, 4
+            //Espresso: 4, 0, 3
+            //Schwarz: 3, 0, 4
+        }
+        public void CheckReserve(int c, int m, int w)
+        {
+            switch ((c, m, w))
+            {
+                case (2, 8, 4):
+                    if (coffee >= 2 && milk >=8 && water >= 4)
+                    {
+                        Console.WriteLine("Latte wird zubereitet.");
+                        Reserve(c, m, w);
+                    }
+                    else if (coffee < 2 && milk >=8 && water >= 4)
+                    {
+                        Console.WriteLine("Bitte erst Kaffee nachfüllen.");
+                        Idle();
+                    }
+                    else if (coffee >= 2 && milk < 8 && water >= 4)
+                    {
+                        Console.WriteLine("Bitte erst Milch nachfüllen.");
+                        Idle();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Bitte erst Wasser nachfüllen.");
+                        Idle();
+                    }
+                    break;
+                case (2, 4, 4):
+                    if (coffee >= 2 && milk >= 4 && water >= 4)
+                    {
+                        Console.WriteLine("Capu wird zubereitet.");
+                        Reserve(c, m, w);
+                    }
+                    else if (coffee < 2 && milk >= 4 && water >= 4)
+                    {
+                        Console.WriteLine("Bitte erst Kaffee nachfüllen.");
+                        Idle();
+                    }
+                    else if (coffee >= 2 && milk < 4 && water >= 4)
+                    {
+                        Console.WriteLine("Bitte erst Milch nachfüllen.");
+                        Idle();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Bitte erst Wasser nachfüllen.");
+                        Idle();
+                    }
+                    break;
+                case (4, 0, 3):
+                    if (coffee >= 3 && water >= 4)
+                    {
+                        Console.WriteLine("Schwarz wird zubereitet.");
+                        Reserve(c, m, w);
+                    }
+                    else if (coffee >= 3 && water < 3)
+                    {
+                        Console.WriteLine("Bitte erst Wasser nachfüllen.");
+                        Idle();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Bitte erst Kaffee nachfüllen.");
+                        Idle();
+                    }
+                    break;
+                    ;
+                case (3, 0, 4):
+                    if (coffee >= 3 && water >= 4)
+                    {
+                        Console.WriteLine("Schwarz wird zubereitet.");
+                        Reserve(c, m, w);
+                    }
+                    else if (coffee >= 3 && water < 4)
+                    {
+                        Console.WriteLine("Bitte erst Wasser nachfüllen.");
+                        Idle();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Bitte erst Kaffee nachfüllen.");
+                        Idle();
+                    }
+                    break;
+
+                default:
+                    break;
+            }
         }
         public void DisplayReserve()
-        {
-            Console.WriteLine("Reserve: Coffee @ " + coffee + "%, Milk @ " + milk + "%, Water @ " + water + "%");
-        }
-        public void CheckReserve()
-        {
-            if (coffee < 2)
-            {
-                Console.WriteLine("Bitte erst Kaffee nachfüllen.");
-                Idle();
-            }
-            if (milk < 8)
-            {
-                Console.WriteLine("Bitte erst Milch nachfüllen.");
-                Idle();
-            }
-            if (water < 4)
-            {
-                Console.WriteLine("Bitte erst Wasser nachfüllen.");
-                Idle();
-            }
-        }
+                {
+                    Console.WriteLine("Reserve: Coffee @ " + coffee + "%, Milk @ " + milk + "%, Water @ " + water + "%");
+                }
         public void Reinigung()
         {
             Console.WriteLine("Reinigung wird gestartet.");
-
+            Idle();
         }
         public void Shutdown()
         {
             Console.WriteLine("Maschine wird ausgeschalten.");
-
         }
     }
 }
